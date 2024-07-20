4af37cb4912d021fe6c3d0a7db20782449c8b8ca
vinodramu <vinodramu@gmail.com>
2024-07-20 12:33:58 +0530

    commit 3

--- frontend/PostGraduation_Registartion-FrontEnd/src/app/app.module.ts
+++ frontend/PostGraduation_Registartion-FrontEnd/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -17,7 +17,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
